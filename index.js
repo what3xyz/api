6d4d25405fa2247ab898757c44bb959a810a137f
Getter Rätsep <getter@hiss.co>
2018-11-13 09:50:33 +0200

    Created index.js, imported express and middleware.

---
+++ index.js
@@ -0,0 +1,5 @@
+// Express and middleware
+const express = require('express');
+const cors = require('cors');
+const {json, urlencoded} = require('body-parser');
+const jwt = require('jsonwebtoken');

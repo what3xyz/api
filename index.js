23b1dce5419b0361fb70af4657670066135a585f
Getter Rätsep <getter@hiss.co>
2018-11-13 09:53:08 +0200

    Added a port, exporting it from /config/api.json. Added a sample api.json file.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const env = require("./config/api.json");
+
 // Express and middleware
 const express = require('express');
 const cors = require('cors');
 const {json, urlencoded} = require('body-parser');
 const jwt = require('jsonwebtoken');
+
+// Listen to Express API
+if(env.PORT) app.listen(env.PORT, () => { console.log(`CORS-enabled API...listening on port ${env.PORT}:`) });
+else throw(new Error(`Please define PORT: ${env.PORT} in config file.`));

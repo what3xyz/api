346a05881d270726e6e46be243bc58cdb736bf44
Getter Rätsep <getter@hiss.co>
2018-11-13 13:47:52 +0200

    Created /auth endpoint for signup and login. Added status 500 error handling for a faulty route.

---
+++ endpoints/auth/index.js
@@ -0,0 +1,32 @@
+/*
+
+        /auth
+
+*/
+
+module.exports = {
+
+    post: async (req, res, next) => {
+
+        try {
+
+            switch(req.params.route) {
+
+                case 'signup':
+                    signup(req, res, next);
+                    break;
+
+                case 'login':
+                    login(req, res, next);
+                    break;
+
+                default:
+                    return next({status: 404});
+
+            }
+        
+        } catch(e) {
+            return next({status: 500, trace: e});
+        }
+    }
+};
--- index.js
+++ index.js
@@ -6,6 +6,34 @@ const cors = require('cors');
 const {json, urlencoded} = require('body-parser');
 const jwt = require('jsonwebtoken');
 
+// Include endpoints
+const auth = require('./endpoints/auth');
+
+
+
+
+let app = express();
+app.use(cors());
+app.use(json());
+app.use(urlencoded({extended: false}));
+
+
+
+
+// API endpoints
+app.post('/auth/:route', auth.post);
+
+
+
+
+
+
+
+
+
+
+
 // Listen to Express API
-if(env.PORT) app.listen(env.PORT, () => { console.log(`CORS-enabled API...listening on port ${env.PORT}:`) });
+if(env.PORT) app.listen(env.PORT, () => { 
+    console.log(`CORS-enabled API...listening on port ${env.PORT}:`) });
 else throw(new Error(`Please define PORT: ${env.PORT} in config file.`));

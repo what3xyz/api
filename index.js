0a9b6607ed984d7df563f97bf86f000d533dbf55
Getter Rätsep <getter@hiss.co>
2018-11-19 13:38:38 +0200

    Updated index.js for better error handling. Catching "/ "endpoint on the api, sending back name and description. Set a namespace for api version 1. Api-sample file update for error handling.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const env = require("./config/api.json");
 
 // Express and middleware
 const express = require('express');
 const cors = require('cors');
 const {json, urlencoded} = require('body-parser');
+
+const env = require("./config/api.json");
 const jwt = require('jsonwebtoken');
+const { errorHandler } = require('./utils/errorHandling');
 
 
 let app = express();
@@ -16,21 +18,28 @@ app.use(urlencoded({extended: false}));
 // Include endpoints
 const auth = require('./endpoints/auth');
 
-
+//API
+let api = express();
 
 
 // API endpoints
-app.post('/auth/:route', auth.post);
+api.post('/auth/:route', auth.post);
+
+
+api.get('/', (req, res) => res.send({version: env.apiVersion}));
+
+// Set API Namespace
+app.use('/v1', api);
+app.get('/', (req, res) => res.send({name: env.apiName, description: env.apiDescription}));
 
 
 
+//APP (Catch-all with JSON responses)
+app.post('*', (req, res, next) => next({status: 404}));
 
 
-
-
-
-
-
+// Set Error Handler
+app.use(errorHandler);
 
 // Listen to Express API
 if(env.PORT) app.listen(env.PORT, () => { 

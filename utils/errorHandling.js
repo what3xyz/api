6a14bec94681812029a9cef75cf190bf04166b5d
Getter Rätsep <getter@hiss.co>
2018-11-19 13:35:27 +0200

    Added errorHandler to send user clean JSON errors and let them know what went wrong with the request. Only statud 400, 404 and 500 for now. To be updated!

---
+++ utils/errorHandling.js
@@ -0,0 +1,28 @@
+const env = require("../config/api.json");
+
+/* 
+
+    errorHandler()
+    This is to send a clean JSON error to the end user so they
+    know what went wrong with their request.
+
+
+*/
+
+function errorHandler(err, req, res, next) {
+
+    let { status, message } = err;
+    status = status || 400;
+    let key = status.toString();
+
+    let messages = {
+        "400": "Bad Request. " + (message || env.apiBadRequest),
+        "404": "Not Found. " + (message || env.apiNotFound),
+        "500": "Internal Server Error. " + (message || env.apiInternalError)
+    };
+    message = messages[key] ? messages[key] : env.apiBadRequest;
+
+    res.status(status).send({status, error: message});
+}
+
+module.exports = { errorHandler };

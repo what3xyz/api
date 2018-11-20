0cbed4b007b49f034e44e9a7952259feebeb2768
Getter Rätsep <getter@hiss.co>
2018-11-20 17:04:27 +0200

    Validating entered fields for updating user info.

--- endpoints/users/put.js
+++ endpoints/users/put.js
@@ -1,4 +1,5 @@
 const db = require('../../utils/db');
+const isEmail = require('isemail').validate;
 
 /*
 
@@ -21,7 +22,6 @@ module.exports = async (req, res, next) => {
         // Only user can edit their own record
         if (userId !== uid) return next({status: 403, message: 'You can only edit your own user record.'});
 
-
         // User can change ONLY (name, email, avatar) NOT (id, etc.)
         let acceptableFields = {
             email: 'string',
@@ -29,8 +29,17 @@ module.exports = async (req, res, next) => {
             name: 'string',
             avatar: 'string'
         };
-
         
+        // Validate Fields
+        if (body.email && !isEmail(body.email)) return next({status: 400, message: `Email address (${body.email}) is not a valid email.`});
+
+        let fields = Object.keys(body);
+        fields.forEach(field => {
+            if (!acceptableFields[field]) return next({status: 400, message: `Cannot update a field named ${field}.`});
+            if (typeof body[field] !== acceptableFields[field]) return next({status: 400, message: `Field (${field}) is not of valid type.`});
+        });
+
+
     } catch(e) {
         return next({status: 500, trace: e});
     }

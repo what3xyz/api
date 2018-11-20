619d64355733e0daf9a9c95865f652a5e420baad
Getter Rätsep <getter@hiss.co>
2018-11-20 16:45:47 +0200

    Checking if user has access to required user records, 403 error messaging in case does not. Set acceptable fields for updating.

--- endpoints/users/put.js
+++ endpoints/users/put.js
@@ -11,7 +11,26 @@ module.exports = async (req, res, next) => {
 
     try {
 
+        let uid = req.headers.uid;
+        let body = req.body;
+        let { userId } = req.params;
 
+        // Make sure a password is never sent by accident.
+        delete body.password;
+
+        // Only user can edit their own record
+        if (userId !== uid) return next({status: 403, message: 'You can only edit your own user record.'});
+
+
+        // User can change ONLY (name, email, avatar) NOT (id, etc.)
+        let acceptableFields = {
+            email: 'string',
+            username: 'string',
+            name: 'string',
+            avatar: 'string'
+        };
+
+        
     } catch(e) {
         return next({status: 500, trace: e});
     }

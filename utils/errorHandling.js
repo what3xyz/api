c083f4e770103deea93e2360c4368567f94fb9c0
Getter Rätsep <getter@hiss.co>
2018-11-20 16:46:16 +0200

    Added status 403 error handling to api-sample and errorHandling.

--- utils/errorHandling.js
+++ utils/errorHandling.js
@@ -18,6 +18,7 @@ function errorHandler(err, req, res, next) {
     let messages = {
         "400": "Bad Request. " + (message || env.apiBadRequest),
         "401": "Unauthorized. " + (message || env.apiUnauthorized),
+        "403": "Forbidden. " + (message || env.apiForbidden),
         "404": "Not Found. " + (message || env.apiNotFound),
         "500": "Internal Server Error. " + (message || env.apiInternalError)
     };

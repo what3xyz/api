78a519a1025bcadcdc450e3b59b0b1e529512f18
Getter Rätsep <getter@hiss.co>
2018-11-21 08:04:59 +0200

    Started query builder for updating user info. Changed terms for "key" and "field" to reflect actual meaning better. forEach loop to be added !

--- endpoints/users/put.js
+++ endpoints/users/put.js
@@ -12,8 +12,8 @@ module.exports = async (req, res, next) => {
 
     try {
 
-        let uid = req.headers.uid;
-        let body = req.body;
+        let { uid } = req.headers;
+        let { body } = req;
         let { userId } = req.params;
 
         // Make sure a password is never sent by accident.
@@ -33,15 +33,36 @@ module.exports = async (req, res, next) => {
         // Validate Fields
         if (body.email && !isEmail(body.email)) return next({status: 400, message: `Email address (${body.email}) is not a valid email.`});
 
-        let fields = Object.keys(body);
-        fields.forEach(field => {
-            if (!acceptableFields[field]) return next({status: 400, message: `Cannot update a field named ${field}.`});
-            if (typeof body[field] !== acceptableFields[field]) return next({status: 400, message: `Field (${field}) is not of valid type.`});
+        let keys = Object.keys(body);
+        keys.forEach(key => {
+            if (!acceptableFields[key]) return next({status: 400, message: `Cannot update a field named ${key}.`});
+            if (typeof body[key] !== acceptableFields[key]) return next({status: 400, message: `Field (${key}) is not of valid type.`});
         });
+
+        /* 
+            Query Builder
+        */
+
+       let field = '', 
+       params = [ new Date(), userId ];
+
+       //  forEach loop here ..
+
+
+       // Query
+       let query =`
+            UPDATE users SET ${field}, modified=$1
+            WHERE active=true AND id=$2
+            RETURNING id, username, email, name, avatar, created, modified
+       `
+
+       let result = await db.query(query, params);
+
+       res.send(result.rows[0]);
 
 
     } catch(e) {
         return next({status: 500, trace: e});
     }
 
-}
+};
